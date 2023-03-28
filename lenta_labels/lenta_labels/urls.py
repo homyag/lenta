c204b3af2fa50189cb12e815b5af2b440ac02e54
homyag <homyaga@gmail.com>
2023-03-28 22:07:31 +0300

    add view in first_page add urls to first_page add index.html with bootstrap cdn

--- lenta_labels/lenta_labels/urls.py
+++ lenta_labels/lenta_labels/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_page import views
 
 urlpatterns = [
+    path('', views.first_page),
     path('admin/', admin.site.urls),
 ]
