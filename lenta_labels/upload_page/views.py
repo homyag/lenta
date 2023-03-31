f1ee136b718bc192c08b49c611489691fa5a61cf
homyag <homyaga@gmail.com>
2023-03-31 15:50:18 +0300

    [v.0.1.1] loads the template file correctly and saves the result in result.xlsx

--- lenta_labels/upload_page/views.py
+++ lenta_labels/upload_page/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-from lenta_labels.settings import UPLOAD_DIR, BASE_DIR
+from lenta_labels.settings import BASE_DIR
 import os
-import pandas as pd
-import io
-from upload_page.forms import UploadFileForm
 from upload_page.utils import get_format
-from django.conf import settings
 
 
 def upload_page(request):
@@ -33,7 +28,7 @@ def upload_file(request):
         get_format(file_path)
 
         # Возвращаем пользователю страницу с сообщением об успешной загрузке
-        return render(request, 'upload_page.html', {'success': 'Файл успешно '
+        return render(request, 'results.html', {'success': 'Файл успешно '
                                                                'загружен и обработан'})
 
     # Если метод запроса GET, то просто возвращаем страницу загрузки файла
