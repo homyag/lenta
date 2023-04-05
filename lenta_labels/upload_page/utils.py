5b48f5f38ec1702281ed9f598ed536474b9c1c43
homyag <homyaga@gmail.com>
2023-04-05 13:13:25 +0700

    added two button on results.html to download two file

--- lenta_labels/upload_page/utils.py
+++ lenta_labels/upload_page/utils.py
@@ -7,6 +7,7 @@ from django.conf import settings
 BASE_DIR = settings.BASE_DIR
 
 
+# функция обработки шаблона во вшивную этикету одежды
 def get_format(file_path):
     file_path = os.path.join(BASE_DIR, 'upload_documents', 'template.xlsx')
     x1 = pd.read_excel(file_path)
@@ -32,6 +33,66 @@ def get_format(file_path):
 
     # Сохраняем файл в директорию upload_documents
     result_file_path = os.path.join(BASE_DIR, 'upload_documents',
-                                    'result.xlsx')
+                                    'lousy_label.xlsx')
     with open(result_file_path, 'wb') as f:
         f.write(output.getbuffer())
+
+
+def get_format_label(file_path):
+    file_path = os.path.join(BASE_DIR, 'upload_documents', 'template.xlsx')
+    x1 = pd.read_excel(file_path)
+    df2 = x1
+    df2['1'] = ""
+    df2['2'] = "Дата изготовления:"
+    df2['3'] = "place for care symbols"
+    df2['4'] = "Страна происхождения: "
+    df2[
+        'Срок службы ограничен физическим износом изделия.'] = 'Срок службы ограничен физическим износом изделия.'
+    df2['артикул модель'] = 'Артикул/модель: '
+    df2['Размер :'] = 'Размер: '
+    df2['Состав :'] = 'Состав : '
+    df2['4'] = "Страна-изготовитель:"
+    df2[
+        '5'] = 'Импортёр, уполномоченная организация на принятие претензий и поставщик в РФ: ООО «ЛЕНТА», Россия, 197374, г. Санкт-Петербург, ул. Савушкина, д. 112, литера Б. Мы ждем ваши отзывы по телефону горячей линии: 8-800-700-4-111 (на территории РФ звонок бесплатный) и по адресу: private.label@lenta.com.'
+    df2['6'] = 'Срок службы ограничен физическим износом изделия.'
+    df2['7'] = 'Изготовитель:'
+    df2['8'] = 'Адрес производства:'
+    df3 = pd.DataFrame()
+    df3 = df2[
+        ['1', 'Бренд', '1', 'Наименование', '1', 'артикул', 'Размер', 'Состав',
+         '3',
+         'ИЗГОТОВИТЕЛЬ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/ Company name, Legal Adress',
+         'АДРЕС РЕАЛЬНОГО ИЗГОТОВИТЕЛЯ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/Adress real factory production  ',
+         'Страна происхождения', '5', '1', '2', '6', '1', 'EAN изделия',
+         '1']].copy()
+    df3['артикул'] = df2['артикул модель'].astype(str) + df3['артикул'].astype(
+        str)
+    df3['Размер'] = df2['Размер :'].astype(str) + df3['Размер'].astype(str)
+    df3['Состав'] = df2['Состав :'].astype(str) + df3['Состав'].astype(str)
+    df3['Страна происхождения'] = df2['4'].astype(str) + df3[
+        'Страна происхождения'].astype(str)
+    df3[
+        'ИЗГОТОВИТЕЛЬ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/ Company name, Legal Adress'] = \
+    df2['7'].astype(str) + df3[
+        'ИЗГОТОВИТЕЛЬ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/ Company name, Legal Adress'].astype(
+        str)
+    df3[
+        'АДРЕС РЕАЛЬНОГО ИЗГОТОВИТЕЛЯ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/Adress real factory production  '] = \
+    df2['8'].astype(str) + df3[
+        'АДРЕС РЕАЛЬНОГО ИЗГОТОВИТЕЛЯ, КОТОРЫЙ  УКАЗАН НА ЭТИКЕТКЕ\n/Adress real factory production  '].astype(
+        str)
+    df3 = df3_transposed = df3.T  # or df1.transpose()
+    df3 = df3_transposed
+    # Записываем результат в io.BytesIO
+    output = io.BytesIO()
+    df3.to_excel(output, index=False, header=False)
+    output.seek(0)
+
+    result_file_path = os.path.join(BASE_DIR, 'upload_documents',
+                                    'label_on_item.xlsx')
+    with open(result_file_path, 'wb') as f:
+        f.write(output.getbuffer())
+
+
+
+
--- lenta_labels/upload_page/views.py
+++ lenta_labels/upload_page/views.py
@@ -1,9 +1,13 @@
-from django.http import FileResponse, Http404, StreamingHttpResponse
+from django.conf import settings
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from lenta_labels.settings import BASE_DIR
 import os
 from upload_page.utils import get_format
+from upload_page.utils import get_format_label
 from django.urls import reverse
+
+# BASE_DIR = settings.BASE_DIR
 
 
 def upload_page(request):
@@ -25,39 +29,61 @@ def upload_file(request):
             for chunk in file.chunks():
                 destination.write(chunk)
 
-        # Вызываем функцию get_format для обработки файла
+        # Вызываем функции для обработки файлов
         get_format(file_path)
+        get_format_label(file_path)
+
 
         # Возвращаем пользователю страницу с сообщением об успешной загрузке
-        result_file_path = os.path.join(BASE_DIR, 'upload_documents',
-                                        'result.xlsx')
-        url = reverse('download_file',
-                      args=[os.path.basename(result_file_path)])
-        download_url = request.build_absolute_uri(url)
+        result_file_path_lousy = os.path.join(BASE_DIR, 'upload_documents',
+                                              'lousy_label.xlsx')
+        url_lousy = reverse('download_file',
+                            args=[os.path.basename(result_file_path_lousy)])
+        download_url_lousy = request.build_absolute_uri(url_lousy)
+
+        result_file_path_mounted = os.path.join(BASE_DIR, 'upload_documents',
+                                                'mounted_label.xlsx')
+        url_mounted = reverse('download_file',
+                              args=[
+                                  os.path.basename(result_file_path_mounted)])
+        download_url_mounted = request.build_absolute_uri(url_mounted)
+
         return render(request, 'results.html',
-                      {'success': 'Файл успешно загружен и обработан',
-                       'download_url': download_url})
+                      {'success': 'Файлы успешно загружены и обработаны',
+                       'download_url_lousy': download_url_lousy,
+                       'download_url_mounted': download_url_mounted})
 
     # Если метод запроса GET, то просто возвращаем страницу загрузки файла
     return render(request, 'upload_page.html')
 
 
-def download_file(request, file_name):
-    """Эта функция download_file представляет собой обработчик запроса, который
-     отправляет пользователю файл, расположенный на сервере по указанному пути
-     file_path.
-    Функция открывает файл в бинарном режиме ('rb') с помощью функции open,
-    создает объект StreamingHttpResponse с содержимым файла в качестве
-    параметра и устанавливает тип содержимого файла
-    'application/vnd.ms-excel'). Затем она устанавливает заголовок
-    Content-Disposition, который сообщает браузеру, что файл должен быть
-    скачан, а не отображен в браузере, и возвращает объект
-    StreamingHttpResponse.
-    Пользователь получает содержимое файла в виде скачиваемого файла с
-    именем file_name."""
-    file_path = os.path.join(BASE_DIR, 'upload_documents', file_name)
-    file = open(file_path, 'rb')
-    response = StreamingHttpResponse(file,
-                                     content_type='application/vnd.ms-excel')
-    response['Content-Disposition'] = f'attachment; filename="{file_name}"'
-    return response
+# def download_file(request, file_name):
+#     """Эта функция download_file представляет собой обработчик запроса, который
+#      отправляет пользователю файл, расположенный на сервере по указанному пути
+#      file_path.
+#     Функция открывает файл в бинарном режиме ('rb') с помощью функции open,
+#     создает объект StreamingHttpResponse с содержимым файла в качестве
+#     параметра и устанавливает тип содержимого файла
+#     'application/vnd.ms-excel'). Затем она устанавливает заголовок
+#     Content-Disposition, который сообщает браузеру, что файл должен быть
+#     скачан, а не отображен в браузере, и возвращает объект
+#     StreamingHttpResponse.
+#     Пользователь получает содержимое файла в виде скачиваемого файла с
+#     именем file_name."""
+#     file_path = os.path.join(BASE_DIR, 'upload_documents', file_name)
+#     file = open(file_path, 'rb')
+#     response = StreamingHttpResponse(file,
+#                                      content_type='application/vnd.ms-excel')
+#     response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+#     return response
+
+def download_file(request, file_names):
+    links = []
+    for file_name in file_names:
+        file_path = os.path.join(BASE_DIR, 'upload_documents', file_name)
+        file = open(file_path, 'rb')
+        response = StreamingHttpResponse(file,
+                                         content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        links.append(response)
+        return render(request, 'results.html', {'links': links})
